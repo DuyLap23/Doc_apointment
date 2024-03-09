7082e6b4321dd1185ac197ceb3221733a56c9677
hoangduylap <hoangduylap124@gmail.com>
2024-03-09 08:35:19 +0700

    upload giao diện

---
+++ common/route.php
@@ -0,0 +1,43 @@
+<?php
+use Phroute\Phroute\RouteCollector;
+
+$url = !isset($_GET['url']) ? "/" : $_GET['url'];
+
+$router = new RouteCollector();
+
+// filter check đăng nhập
+$router->filter('auth', function(){
+    if(!isset($_SESSION['auth']) || empty($_SESSION['auth'])){
+        header('location: ' . BASE_URL . 'login');die;
+    }
+   
+
+});
+$router->get('/', function(){
+    return "Home";
+});
+// khu vực cần quan tâm -----------
+// bắt đầu định nghĩa ra các đường dẫn
+// $router->get('/', [App\Controllers\BaseController::class,'home']);
+//định nghĩa đường dẫn trỏ đến Product Controller
+// $router->get('list-product',[App\Controllers\ProductController::class,'index']);
+// $router->get('add-product',[App\Controllers\ProductController::class,'addProduct']);
+// $router->get('delete-product/{id_product}',[App\Controllers\ProductController::class,'delProduct']);
+// $router->post('post-product',[App\Controllers\ProductController::class,'postProduct']);
+// $router->post('edit-product/{id_product}',[App\Controllers\ProductController::class,'editProduct']);
+// $router->get('detail-product/{id_product}',[App\Controllers\ProductController::class,'detail']);
+
+// $router->post('edit-product/{id}',[App\Controller\ProductController::class,'editProduct']);
+// khu vực cần quan tâm -----------
+//$router->get('test', [App\Controllers\ProductController::class, 'index']);
+
+# NB. You can cache the return value from $router->getData() so you don't have to create the routes each request - massive speed gains
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $url);
+
+// Print out the value returned from the dispatched function
+echo $response;
+
+
+?>
---
+++ env.php
@@ -0,0 +1,13 @@
+<?php
+const DBNAME = '';
+const DBUSER = 'root';
+const DBPASS = '';
+const DBHOST = '127.0.0.1';
+const DBCHARSET = 'utf8';
+
+const BASE_URL = 'http://localhost/doc_apointment/';
+
+function route($url){
+    return BASE_URL . $url;
+}
+?>
