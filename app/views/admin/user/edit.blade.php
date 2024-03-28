cbd5df7f2066de923a850c391326cc734af05df9
thuanvm04 <vuminhthuan004@gmail.com>
2024-03-28 10:03:37 +0700

    fix

--- app/views/admin/user/edit.blade.php
+++ app/views/admin/user/edit.blade.php
@@ -10,47 +10,74 @@
     @if(isset($_SESSION['success']) && isset($_GET['msg']))
         <span style="color: green">{{ $_SESSION['success'] }}</span>
     @endif
-    
-    <form action="{{ route('admin/user/edit/'.$user->id) }}" enctype="multipart/form-data" method="POST">
+    <main>
+        <div class="head-title">
+            <div class="left">
+                <h1>Chuyên môn</h1>
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="#">Trang chủ</a>
+                    </li>
+                    <li><i class='bx bx-chevron-right'></i></li>
+                    <li>
+                        <a class="" href="#">Tài khoản </a>
+                    </li>
+                    <li><i class='bx bx-chevron-right'></i></li>
+                    <li>
+                        <a class="active" href="#">Sửa tài khoản bác sĩ</a>
+                    </li>
+                </ul>
+            </div>
 
-        <table>
-            <tr>
-                <td>Email</td>
-                <td><input type="text" name="email" value="{{ $user->email }}"/></td>
-            </tr>
-            <tr>
-                <td>Mật khẩu</td>
-                <td><input type="text" name="password" value="{{ $user->password }}"/></td>
-            </tr>
-            <tr>
-                <td>Họ</td>
-                <td><input type="text" name="firstName" value="{{ $user->firstName }}"/></td>
-            </tr>
-            <tr>
-                <td>Tên</td>
-                <td><input type="text" name="lastName" value="{{ $user->lastName }}"/></td>
-            </tr>
-            <tr>
-                <td>Số điện thoại</td>
-                <td><input type="text" name="phonenumber" value="{{ $user->phonenumber }}"/></td>
-            </tr>
-            <tr>
-                <td>Địa chỉ</td>
-                <td><input type="text" name="address" value="{{ $user->address }}"/></td>
-            </tr>
-            <tr>
-                <td>Giới tính</td>
-                <td><input type="text" name="gender" value="{{ $user->gender }}"/></td>
-            </tr>
-            <tr>
-                <td>Ảnh đại diện</td>
-                <img src="../../../images/{{ $user->image }}" alt="">
-                <td><input type="file" name="image" ></td>
-            </tr>
+        </div>
 
-            <tr>
-                <td><input type="submit" name="edit" value="Sửa" /></td>
-            </tr>
-        </table>
+        <div class="table-data">
+            <div class="order">
+                <div class="head">
+                    <h3>Chỉnh sửa</h3>
+                    </div>
+    <form action="{{ route('admin/user/edit/'.$user->id) }}" enctype="multipart/form-data" method="POST" class="form-input">
+
+             <div class="form-group pb-4">
+             <label for="">Email</label> <br>
+                <input type="text" class="input w-75 rounded-2" name="email" value="{{ $user->email }}"/> <br>
+           
+                <label for="" class="label pt-2">Mật khẩu</label> <br>
+
+                <input type="text" class="input w-75 rounded-2" name="password" value="{{ $user->password }}"/> <br>
+            
+            <label for="" class="label pt-2">Họ</label> <br>
+                <input type="text" class="input w-75 rounded-2" name="firstName" value="{{ $user->firstName }}"/> <br>
+          
+            <label for="" class="label pt-2">Tên</label> <br>
+                <input type="text" class="input w-75 rounded-2" name="lastName" value="{{ $user->lastName }}"/> <br>
+           
+            <label for="" class="label pt-2">Số điện thoại</label> <br>
+                <input type="text" class="input w-75 rounded-2" name="phonenumber" value="{{ $user->phonenumber }}"/> <br>
+          
+            <label for="" class="label pt-2">Địa chỉ</label> <br>
+                <input type="text" class="input w-75 rounded-2 "name="address" value="{{ $user->address }}"/>  <br>
+           
+            <label for="" class="label pt-2">Giới tính</label> <br>
+                <input type="text" class="input w-75 rounded-2 "name="gender" value="{{ $user->gender }}"/> <br>
+           
+            <label for="" class="label pt-2">Ảnh</label> <br>
+                <img src="../../../images/{{ $user->image }}"  width="150px" alt=""> <br>
+                <input  type="file" name="image" > <br>
+             </div>
+           
+
+             <input type="submit" name="submit" id="" value="Cập Nhật"
+                        class="btn btn-insert  status completed ">
+                    <input type="reset" name="reset" id="" value="Nhập Lại" class=" btn btn-reset ">
+                </form>
+                <a href="{{ route('admin/specialty/list') }}"><button class="btn btn-insert  status completed mt-4">Các
+                        chuyên môn
+                    </button></a>
+          
+       
     </form>
+    </div>
+        </div>
+    </main>
 @endsection
